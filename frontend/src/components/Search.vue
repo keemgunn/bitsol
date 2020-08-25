ab6c35a5324ade14cf36c5a0a15550891682ff6a
gunn <gunnkeem@icloud.com>
2020-08-25 17:04:24 +0900

    all set: ready to form records

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -10,10 +10,17 @@
     <ScopeBtn/>
   </div>
 
-  <div id="list">
-
-
+  <div id="list-wrapper">
+    <div id="list">
+      <div class="records">
+        <Record />
+        <div class="record-cover-bottom"></div>
+      </div>
+      
+    </div>
+    <div id="cover-bottom" :style="search.coverBottom"></div>
   </div>
+  <div class="cover-scroll-bottom"></div>
 
 
 
@@ -33,19 +40,20 @@
 <script>
 import ScopeBtn from '@/components/search/ScopeBtn';
 import Deadline from '@/components/search/Deadline';
+import Record from '@/components/search/Record';
 import { mapState, mapMutations } from 'vuex';
 // import axios from 'axios';
 
 export default {
   name: "Search",
   components: {
-    ScopeBtn, Deadline
+    ScopeBtn, Deadline, Record
   },
   data() { return {
 
   }},
   computed: {
-    ...mapState(['search', 'searchArr']),
+    ...mapState(['search', 'searchArr', 'testArr']),
   },
   methods: {
     ...mapMutations(['searchLoadingState']),
@@ -77,6 +85,7 @@ export default {
   align-items: center;
   user-select: none; -webkit-user-select: none;
 }
+
 #content {
   position: relative; top: 0; left: 0;
   z-index: 10;
@@ -84,27 +93,8 @@ export default {
     min-width: 490px;
     max-width: 710px;
   height: 100%;
-  background-color: rgb(0, 224, 150);
+  // background-color: rgb(0, 224, 150);
 }
-
-#option {
-  position: absolute; top: 0; left: 0;
-  width: 100%;
-  height: 86px;
-  background-color: var(--i94);
-}
-
-#list {
-  position: absolute; top: 86px; left: 0;
-  width: 100%;
-  height: calc(100% - 86px);
-  // background-color: rgba(255, 0, 191, 0.493);
-}
-
-
-
-
-
 .cover-app-side {
   position: absolute; top: 0;
   z-index: 2;
@@ -125,5 +115,92 @@ export default {
   .cover-app-side {
       width: calc( 50vw - 245px );
   } 
+}
+
+
+
+
+
+
+#option {
+  position: absolute; top: 0; left: 0;
+  width: 100%;
+  height: 86px;
+  background-color: var(--i94);
+}
+
+
+#list-wrapper {
+  position: absolute; top: 86px; left: -20px;
+  width: calc(100% + 40px);
+  height: calc(100% - 86px);
+  overflow-y: auto;
+  overflow-x: hidden;
+  background-color: rgba(119, 119, 119, 0.24);
+}
+
+#list {
+  position: relative; top: 0; left: 20px;
+  width: calc(100vw - 120px);
+    min-width: 490px;
+    max-width: 710px;
+  height: fit-content;
+  background-color: #24deff56;
+}
+
+.records {
+  position: relative; top: 0; left: 0;
+  width: 100%;
+  height: fit-content;
+  .record-cover-bottom {
+    position: relative; top: 0; left: 0;
+    width: 100%;
+    height: 10px;
+    background-color: var(--i94);
+    background-color: rgba(121, 121, 121, 0.247);
+  }
+}
+
+#cover-bottom {
+  width: 100%;
+  background-color: var(--i94);
+  background-color: rgba(0, 100, 0, 0.404);
+}
+
+/* --------------- SCROLL -------------- */
+/* width */
+::-webkit-scrollbar {
+  transition: 100ms;
+  width: 5px;
+  max-height: 70%;
+  height: 70%;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: var(--i70);
+  border: 1px solid var(--i94);
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: var(--i45);
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: var(--i30);
+}
+::-webkit-scrollbar-track-piece:start {
+  background: transparent;
+  margin-top: 0px;
+}
+::-webkit-scrollbar-track-piece:end {
+  background: transparent;
+  margin-bottom: 50px; 
+}
+.cover-scroll-bottom {
+  position: absolute; bottom: 0px; right: -20px;
+  float: right;
+  width: 5px;
+  height: 50px;
+  background-color: var(--i94);
 }
 </style>
---
+++ frontend/src/components/Search/Record.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="record">
+  <div class="plate">
+
+
+
+
+  </div>
+</div>
+</template>
+
+
+
+<script>
+export default {
+  name: "Record",
+  props: [
+
+  ],
+  data() { return {
+
+  }},
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+    
+  },
+  beforeUpdate() {
+    
+  },
+  beforeCreate() {
+    
+  },
+}
+</script>
+
+
+
+<style lang="scss" scoped> 
+.record {
+  position: relative; top: 0; left: 0;
+  width: 100%;
+  height: 56px;
+  background-color: darksalmon;
+}
+.plate {
+  position: relative; top: 3px; left: 3px;
+  width: calc(100% - 6px);
+  height: 50px;
+  background-color: var(--i94);
+}
+
+
+
+
+
+
+</style>
